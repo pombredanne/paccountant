66e7e4126ef9ea344157417cdba26d98e516927c
Peter Waller <peter@scraperwiki.com>
2013-11-07 11:07:50 +0000

    Serialize to JSON

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,8 +10,61 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
+	"syscall"
 )
+
+type Process struct {
+	Cmdline, Pwd, Exe string
+	Status            int
+	Uid               int64
+	Memory            struct {
+		Maxrss uint32
+	}
+	Io struct {
+		Nreads, Nwrites uint64
+		Byter, Bytew    uint64
+		Blockr, Blockw  uint64 // To block devices
+	}
+}
+
+func makeDict(input string) map[string]string {
+	result := map[string]string{}
+	for _, line := range strings.Split(input, "\n") {
+		leftright := strings.SplitN(line, ":", 2)
+		if len(leftright) < 2 {
+			continue
+		}
+		result[leftright[0]] = strings.TrimSpace(leftright[1])
+	}
+	return result
+}
+
+func NewProcess(status int, cmdline, pwd, exe, io, stat string) Process {
+	iodict := makeDict(io)
+	statdict := makeDict(stat)
+
+	process := Process{
+		Cmdline: cmdline,
+		Status:  status,
+		Pwd:     pwd,
+		Exe:     exe,
+	}
+
+	fmt.Sscan(statdict["Uid"], &process.Uid)
+
+	fmt.Sscan(statdict["VmHWM"], &process.Memory.Maxrss)
+
+	fmt.Sscan(iodict["rchar"], &process.Io.Byter)
+	fmt.Sscan(iodict["wchar"], &process.Io.Bytew)
+	fmt.Sscan(iodict["read_bytes"], &process.Io.Blockr)
+	fmt.Sscan(iodict["write_bytes"], &process.Io.Blockw)
+	fmt.Sscan(iodict["syscr"], &process.Io.Nreads)
+	fmt.Sscan(iodict["syscw"], &process.Io.Nwrites)
+
+	return process
+}
 
 func check(err error) {
 	if err != nil {
@@ -18,12 +72,15 @@ func check(err error) {
 	}
 }
 
-func serveOne(conn net.Conn, data chan<- []byte) {
+func serveOne(conn net.Conn, data chan<- Process) {
 	// Ensure that ``conn`` is closed on matter how we exit serveOne()
-	defer func() {
+	exit := func() {
+		// log.Println("Done")
 		err := conn.Close()
 		check(err)
-	}()
+	}
+
+	defer exit()
 
 	b := make([]byte, 32)
 	n, err := conn.Read(b)
@@ -46,43 +103,64 @@ func serveOne(conn net.Conn, data chan<- []byte) {
 	status_content, err := ioutil.ReadFile(proc + "status")
 	check(err)
 
-	cmdline, err := ioutil.ReadFile(proc + "cmdline")
+	cmdline_content, err := ioutil.ReadFile(proc + "cmdline")
+	check(err)
+
+	pwd, err := os.Readlink(proc + "cwd")
+	check(err)
+	exe, err := os.Readlink(proc + "exe")
 	check(err)
 
 	go func() {
-		data <- append(status_content, append(io_content, cmdline...)...)
+		data <- NewProcess(status, string(cmdline_content), pwd, exe,
+			string(io_content), string(status_content))
 	}()
 }
 
-func writelog(data <-chan []byte, done <-chan struct{}) {
+func writelog(data <-chan Process, done <-chan struct{}, hup <-chan os.Signal) {
 	defer println("Written log")
 
-	fd, err := os.Create("paccountant.log")
+	filename := "paccountant.log"
+
+	flags := os.O_CREATE | os.O_WRONLY | os.O_APPEND
+	fd, err := os.OpenFile(filename, flags, 0666)
 	check(err)
-	defer fd.Close()
+
+	defer func() { fd.Close() }()
 
 	buf := &bytes.Buffer{}
-	var datum []byte
 
 	for {
 		select {
-		case datum = <-data:
+		case <-hup:
+			log.Println("SIGHUP")
+			err = fd.Close()
+			check(err)
+			fd, err = os.OpenFile(filename, flags, 0666)
+			check(err)
+			continue
+
 		case <-done:
 			log.Println("Recv <-done")
 			return
+
+		case datum := <-data:
+			bytes, err := json.Marshal(&datum)
+			check(err)
+
+			n, err := buf.Write(bytes)
+			check(err)
+
+			buf.WriteByte('\n')
+
+			if n != len(bytes) {
+				check(fmt.Errorf("Unexpected number of bytes to buffer.. %v != %v",
+					n, len(bytes)))
+			}
+
+			_, err = io.Copy(fd, buf)
+			check(err)
 		}
-
-		n, err := buf.Write(datum)
-		check(err)
-		if n != len(datum) {
-			check(fmt.Errorf("Unexpected number of bytes to buffer.. %v != %v",
-				n, len(datum)))
-		}
-
-		_, err = io.Copy(fd, buf)
-		check(err)
-
-		fd.Write([]byte{0})
 	}
 }
 
@@ -91,7 +169,7 @@ func main() {
 	check(err)
 	defer listener.Close()
 
-	logChan := make(chan []byte)
+	logChan := make(chan Process)
 	done := make(chan struct{})
 
 	var wg sync.WaitGroup
@@ -100,7 +178,10 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		writelog(logChan, done)
+
+		hup := make(chan os.Signal)
+		signal.Notify(hup, syscall.SIGHUP)
+		writelog(logChan, done, hup)
 	}()
 
 	go func() {
@@ -120,7 +201,7 @@ func main() {
 	}()
 
 	s := make(chan os.Signal)
-	signal.Notify(s)
+	signal.Notify(s, os.Interrupt)
 	<-s
 
 	close(done)
